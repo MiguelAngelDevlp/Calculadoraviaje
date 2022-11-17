0b3c2a3951ad447b93c5c233f4043d69a30d32c7
MiguelAngeldevlp <miguel.angel01moya@gmail.com>
2022-11-17 21:24:03 +0100

    calculadora

---
+++ main.js
@@ -0,0 +1,46 @@
+function costeHotel(num) {
+    return num*140
+}
+
+function costeAvion(name,num) {
+    const result = 0
+if (name === "Oviedo") {
+    result = num*15
+    
+} else if (name === "Tokio") {
+    result = num*700
+} else if(name === "Madrid" || name === "Barcelona"){
+    result = num *90
+}
+return result
+if (num > 3) {
+    return result-(result*0.1)
+}
+}
+
+function AlquilerCoche(num=40) {
+    const  result = 0
+  if (num >= 3 && num <=6) {
+    result = (num*40)-20
+    return result
+  } else if(num >=7 ){
+     result = (num*40)-50
+     return result
+  }
+}
+
+const coches = document.querySelector(".coches")
+const option = document.querySelector(".opciones")
+const send = document.querySelector(".enviar")
+const costHotel = document.querySelector("#mail")
+const costAvion = document.querySelector("#mail2")
+const costAlquiler = document.querySelector("#mail3")
+const costTotal = document.querySelector("#mail4")
+
+function calcularCost() {
+    send.onclick = () =>{
+     const inputValue = coches.value + 
+     costTotal.textContent = inputValue
+    }
+}
+
